3e99f63d56ffd0a45e04ef42867038284f971a79
Ivan Notaros <nothke@gmail.com>
2018-10-30 13:34:40 +0100

    Initial

---
+++ Assets/CreaseOutline/CreaseOutline.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+namespace Nothke.CreaseOutline
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    public class CreaseOutline : PostEffectsBase
+    {
+        public Shader multShader = null;
+        private Material multMaterial = null;
+
+        //public RenderTexture r2t;
+        RenderTexture RT;
+        public Camera renderCamera;
+
+        public Color outlineColor;
+
+        public override bool CheckResources()
+        {
+            CheckSupport(true);
+
+            multMaterial = CheckShaderAndCreateMaterial(multShader, multMaterial);
+            //multMaterial.SetTexture("_MultTex", r2t);
+            multMaterial.SetColor("_OutlineColor", outlineColor);
+
+            if (!isSupported)
+                ReportAutoDisable();
+            return isSupported;
+        }
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (CheckResources() == false)
+            {
+                Graphics.Blit(source, destination);
+                return;
+            }
+
+            int rtW = source.width;
+            int rtH = source.height;
+
+            if (!RT)
+            {
+                RT = new RenderTexture(rtW, rtH, 0);
+                multMaterial.SetTexture("_MultTex", RT);
+                renderCamera.targetTexture = RT;
+            }
+
+            Graphics.Blit(source, destination, multMaterial);
+
+            /*
+            RenderTexture.ReleaseTemporary(hrTex);
+            RenderTexture.ReleaseTemporary(lrTex1);*/
+        }
+    }
+}
---
+++ Assets/CreaseOutline/Shaders/CreaseOutlineApply.shader
@@ -0,0 +1,69 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Hidden/CreaseOutlineApply"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_MultTex("MultTex", 2D) = "white" {}
+		_OutlineColor("Outline Color", 2D) = "white" {}
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+			sampler2D _MultTex;
+
+			half3 _OutlineColor;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				//col = saturate(col - 0.5);
+				fixed4 col2 = tex2D(_MultTex, i.uv);
+				// just invert the colors
+
+				float crease = col2.r;
+				crease = 1 - crease;
+
+				//col.g += crease;
+				//col.b *= 1 - crease;
+
+				col.rgb = lerp(col.rgb, _OutlineColor, crease);
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
